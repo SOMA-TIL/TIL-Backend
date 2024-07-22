2c29a4e3c1d54a49e1bef6ebfee4c9dd9f4d8179
CodeDiary18 <codediary18@gmail.com>
2024-07-23 00:31:19 +0900

    refactor: 불필요한 throws 제거

--- til-api/src/main/java/com/til/config/security/AuthenticationDeniedHandler.java
+++ til-api/src/main/java/com/til/config/security/AuthenticationDeniedHandler.java
@@ -1,13 +1,10 @@
 package com.til.config.security;
 
-import java.io.IOException;
-
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +17,7 @@ public class AuthenticationDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
-        AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        AccessDeniedException accessDeniedException) {
         handlerExceptionResolver.resolveException(request, response, null, accessDeniedException);
     }
 }
