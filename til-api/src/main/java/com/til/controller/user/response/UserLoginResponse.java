469e2afa9b7989cdf864ec616c4277f53effba05
CodeDiary18 <codediary18@gmail.com>
2024-07-23 01:13:30 +0900

    refactor: 로그인 성공시 사용자 정보 반환 추가
    - 현재는 닉네임만 반환

--- til-api/src/main/java/com/til/controller/user/response/UserLoginResponse.java
+++ til-api/src/main/java/com/til/controller/user/response/UserLoginResponse.java
@@ -1,12 +1,15 @@
 package com.til.controller.user.response;
 
 import com.til.domain.auth.dto.AuthTokenDto;
+import com.til.domain.auth.dto.AuthUserInfoDto;
+import com.til.domain.user.dto.UserPublicInfoDto;
 
 public record UserLoginResponse(
+                                UserPublicInfoDto user,
                                 AuthTokenDto token
 ) {
 
-    public static UserLoginResponse of(AuthTokenDto authTokenDto) {
-        return new UserLoginResponse(authTokenDto);
+    public static UserLoginResponse of(AuthUserInfoDto userInfoDto, AuthTokenDto authTokenDto) {
+        return new UserLoginResponse(UserPublicInfoDto.of(userInfoDto), authTokenDto);
     }
 }
