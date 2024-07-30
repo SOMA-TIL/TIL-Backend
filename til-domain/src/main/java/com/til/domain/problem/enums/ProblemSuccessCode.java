10b7ce3ab080fc4b0567c445f51ab6236acb1bf4
DongYeon Kang <myjjue00@gmail.com>
2024-07-30 13:10:31 +0900

    refactor: Success_code 이름 수정
    - SUCCESS_GET_PROBLEMS -> SUCCESS_GET_PROBLEM_LIST 으로 리스트명 통일

--- til-domain/src/main/java/com/til/domain/problem/enums/ProblemSuccessCode.java
+++ til-domain/src/main/java/com/til/domain/problem/enums/ProblemSuccessCode.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ProblemSuccessCode implements SuccessCode {
 
-    SUCCESS_GET_PROBLEMS("문제 리스트를 성공적으로 가져왔습니다."),
+    SUCCESS_GET_PROBLEM_LIST("문제 리스트를 성공적으로 가져왔습니다."),
     SUCCESS_GET_PROBLEM_INFO("문제 상세 정보를 성공적으로 가져왔습니다.");
 
     private final String message;
