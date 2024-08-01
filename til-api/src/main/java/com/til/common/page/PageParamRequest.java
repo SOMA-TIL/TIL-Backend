344e637f6bf2417b1063a2b441ffbd678c3a91ce
DongYeon Kang <myjjue00@gmail.com>
2024-08-01 16:53:07 +0900

    refactor: 문제 리스트 파라미터 검증 수정

--- til-api/src/main/java/com/til/common/page/PageParamRequest.java
+++ til-api/src/main/java/com/til/common/page/PageParamRequest.java
@@ -9,14 +9,26 @@ public record PageParamRequest(
                                Integer page,
                                Integer size,
                                String sort,
-                               String order) {
+                               String order
+) {
+
+    public PageParamRequest {
+        page = page != null ? page : 0;
+        size = size != null ? size : 10;
+        sort = sort != null ? sort : "id";
+        order = order != null ? order : "asc";
+    }
+
+    public static PageParamRequest of(Integer page, Integer size, String sort, String order) {
+        return new PageParamRequest(page, size, sort, order);
+    }
 
     public PageParamDto toServiceDto() {
         return PageParamDto.builder()
-            .page(this.page != null ? this.page : 0)
-            .size(this.size != null ? this.size : 10)
-            .sort(this.sort != null ? this.sort : "id")
-            .order(this.order != null ? this.order : "asc")
+            .page(this.page)
+            .size(this.size)
+            .sort(this.sort)
+            .order(this.order)
             .build();
     }
 }
