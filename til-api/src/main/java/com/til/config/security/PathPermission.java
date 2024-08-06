aa2d8f2de2a8684e305f8621fb38b949b6a7f429
CodeDiary18 <codediary18@gmail.com>
2024-08-07 02:28:13 +0900

    refactor: 누락된 public 경로 추가

--- til-api/src/main/java/com/til/config/security/PathPermission.java
+++ til-api/src/main/java/com/til/config/security/PathPermission.java
@@ -1,12 +1,25 @@
 package com.til.config.security;
 
+import java.util.stream.Stream;
+
+import org.springframework.util.AntPathMatcher;
+
 public class PathPermission {
 
+    private static final AntPathMatcher pathMatcher = new AntPathMatcher();
+
     public static String[] getPublicPath() {
-        return new String[]{"/user/join", "/user/login", "/user/check-nickname/**"};
+        return new String[]{
+            "/user/join", "/user/login", "/user/check-nickname/**",
+            "/problem", "/problem/{id}", "/category"};
     }
 
     public static String[] getAdminPath() {
         return new String[]{};
+    }
+
+    public static boolean isPublicPath(String path) {
+        return Stream.of(PathPermission.getPublicPath())
+            .anyMatch(pattern -> pathMatcher.match(pattern, path));
     }
 }
