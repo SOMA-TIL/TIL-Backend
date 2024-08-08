66f221f8ae9a35f0a9e98aa3c7e911fd66886c59
doburn <kwon2852@gmail.com>
2024-08-08 22:56:33 +0900

    refactor: Category 객체가 id값도 같이 반환/전송하도록 리팩토링

--- til-domain/src/main/java/com/til/domain/category/dto/CategoryDto.java
+++ til-domain/src/main/java/com/til/domain/category/dto/CategoryDto.java
@@ -6,11 +6,13 @@ import lombok.Builder;
 
 @Builder
 public record CategoryDto(
+                          Long id,
                           String tag
 ) {
 
     public static CategoryDto of(Category category) {
         return CategoryDto.builder()
+            .id(category.getId())
             .tag(category.getTag())
             .build();
     }
