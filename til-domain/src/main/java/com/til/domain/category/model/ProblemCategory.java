8b2b98d88801a9f7bb35eb38ec2a69fe2608c003
CodeDiary18 <codediary18@gmail.com>
2024-08-13 13:58:10 +0900

    refactor: ProblemCategory @GeneratedValue 누락 수정

--- til-domain/src/main/java/com/til/domain/category/model/ProblemCategory.java
+++ til-domain/src/main/java/com/til/domain/category/model/ProblemCategory.java
@@ -4,6 +4,8 @@ import com.til.domain.common.model.BaseTimeEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -19,6 +21,7 @@ import lombok.experimental.SuperBuilder;
 public class ProblemCategory extends BaseTimeEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
