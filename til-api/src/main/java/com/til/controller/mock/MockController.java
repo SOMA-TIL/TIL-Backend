80054bc60d49fdc8be25e6b432e3d05c71945770
CodeDiary18 <codediary18@gmail.com>
2024-08-21 02:51:02 +0900

    chore: 채점 mock api 결과 랜덤 반환

--- til-api/src/main/java/com/til/controller/mock/MockController.java
+++ til-api/src/main/java/com/til/controller/mock/MockController.java
@@ -1,6 +1,7 @@
 package com.til.controller.mock;
 
 import java.util.Map;
+import java.util.Random;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,9 @@ public class MockController {
 
     @PostMapping("/grading-service/grading")
     public ResponseEntity<Object> mockGradingService() {
-        Map<String, Object> response = Map.of("result", "PASS", "comment", "제출한 답변에 대한 피드백입니다.");
-        return ResponseEntity.ok(response);
+        Map<String, Object> pass = Map.of("result", "PASS", "comment", "결과는 PASS이고 다음은 제출한 답변에 대한 피드백입니다.");
+        Map<String, Object> fail = Map.of("result", "FAIL", "comment", "결과는 FAIL이고 제출한 답변에 대한 피드백입니다.");
+
+        return ResponseEntity.ok((new Random()).nextBoolean() ? pass : fail);
     }
 }
