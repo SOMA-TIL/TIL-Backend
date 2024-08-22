22c78244b18397cc3da156918b1fb0048e9f0187
doburn <kwon2852@gmail.com>
2024-08-23 01:28:03 +0900

    refactor: 모의면접 정보 GET API에서 면접 생성 시간 받도록 수정

--- til-domain/src/main/java/com/til/domain/interview/dto/InterviewInfoDto.java
+++ til-domain/src/main/java/com/til/domain/interview/dto/InterviewInfoDto.java
@@ -1,18 +1,21 @@
 package com.til.domain.interview.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import lombok.Builder;
 
 @Builder
 public record InterviewInfoDto(
+                               LocalDateTime createdDate,
                                List<Long> categoryIdList,
                                List<InterviewProblemQuestionDto> problemList
 ) {
 
-    public static InterviewInfoDto of(List<Long> categoryIdList,
+    public static InterviewInfoDto of(LocalDateTime createdDate, List<Long> categoryIdList,
         List<InterviewProblemQuestionDto> problemList) {
         return InterviewInfoDto.builder()
+            .createdDate(createdDate)
             .categoryIdList(categoryIdList)
             .problemList(problemList)
             .build();
