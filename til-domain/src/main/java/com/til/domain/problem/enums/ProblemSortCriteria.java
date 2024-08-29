306c6ed63a29de6869b12eb0d5f15b12aeea57cc
DongYeon Kang <myjjue00@gmail.com>
2024-08-29 23:23:11 +0900

    refactor: 정렬 필드 enum 분리

---
+++ til-domain/src/main/java/com/til/domain/problem/enums/ProblemSortCriteria.java
@@ -0,0 +1,25 @@
+package com.til.domain.problem.enums;
+
+public enum ProblemSortCriteria {
+
+    ID("id"),
+    TITLE("title"),
+    LEVEL("level"),
+    PASSED_COUNT("passedCount"),
+    PASS_RATE("passRate");
+
+    private final String fieldName;
+
+    ProblemSortCriteria(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public static ProblemSortCriteria fromString(String fieldName) {
+        for (ProblemSortCriteria field : ProblemSortCriteria.values()) {
+            if (field.fieldName.equalsIgnoreCase(fieldName)) {
+                return field;
+            }
+        }
+        throw new IllegalArgumentException("정렬할 수 없는 속성입니다: " + fieldName);
+    }
+}
