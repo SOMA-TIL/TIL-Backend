1488a6eda39392850eb1d3d5f8f821636fda6bd9
DongYeon Kang <myjjue00@gmail.com>
2024-10-01 21:18:07 +0900

    refactor: @CurrentUser Long 타입으로 변경

--- til-admin-api/src/main/java/com/til/controller/problem/ProblemController.java
+++ til-admin-api/src/main/java/com/til/controller/problem/ProblemController.java
@@ -19,7 +19,6 @@ import com.til.controller.problem.request.SearchProblemRequest;
 import com.til.controller.problem.request.UpdateProblemRequest;
 import com.til.controller.problem.response.ProblemInfoResponse;
 import com.til.controller.problem.response.ProblemPageResponse;
-import com.til.domain.auth.dto.AuthUserInfoDto;
 import com.til.domain.problem.dto.AdminProblemInfoDto;
 import com.til.domain.problem.dto.AdminProblemListDto;
 import com.til.domain.problem.dto.ProblemPageDto;
@@ -38,17 +37,17 @@ public class ProblemController {
     private final AdminProblemService adminProblemService;
 
     @PostMapping("")
-    public ApiResponse<Void> createProblem(@CurrentUser AuthUserInfoDto adminInfo,
+    public ApiResponse<Void> createProblem(@CurrentUser Long adminId,
         @RequestBody @Valid CreateProblemRequest createProblemRequest) {
-        log.debug("문제 등록 요청 - 관리자 ID: {}", adminInfo.id());
+        log.debug("문제 등록 요청 - 관리자 ID: {}", adminId);
         adminProblemService.createProblem(createProblemRequest.toServiceDto());
         return ApiResponse.ok();
     }
 
     @GetMapping("")
-    public ApiResponse<ProblemPageResponse> readProblemList(@CurrentUser AuthUserInfoDto adminInfo,
+    public ApiResponse<ProblemPageResponse> readProblemList(@CurrentUser Long adminId,
         @ModelAttribute PageParamRequest pageParamRequest, @ModelAttribute SearchProblemRequest searchProblemRequest) {
-        log.debug("문제 리스트 조회 요청 - 관리자 ID: {}", adminInfo.id());
+        log.debug("문제 리스트 조회 요청 - 관리자 ID: {}", adminId);
         ProblemPageDto<AdminProblemListDto> problemPage = adminProblemService.getProblemList(
             pageParamRequest.toServiceDto(), searchProblemRequest.toServiceDto());
         return ApiResponse.ok(ProblemSuccessCode.SUCCESS_GET_PROBLEM_LIST,
@@ -57,24 +56,24 @@ public class ProblemController {
 
     @GetMapping("/{id}")
     public ApiResponse<ProblemInfoResponse> readProblemInfo(
-        @CurrentUser AuthUserInfoDto adminInfo, @PathVariable Long id) {
-        log.debug("문제 조회 요청 - 관리자 ID: {}, 문제 ID: {}", adminInfo.id(), id);
+        @CurrentUser Long adminId, @PathVariable Long id) {
+        log.debug("문제 조회 요청 - 관리자 ID: {}, 문제 ID: {}", adminId, id);
         AdminProblemInfoDto adminProblemInfoDto = adminProblemService.getProblemInfo(id);
         return ApiResponse.ok(ProblemSuccessCode.SUCCESS_GET_PROBLEM_INFO, ProblemInfoResponse.of(adminProblemInfoDto));
     }
 
     @PutMapping("/{id}")
-    public ApiResponse<Void> updateProblem(@CurrentUser AuthUserInfoDto adminInfo,
+    public ApiResponse<Void> updateProblem(@CurrentUser Long adminId,
         @PathVariable Long id,
         @RequestBody @Valid UpdateProblemRequest updateProblemRequest) {
-        log.debug("문제 수정 요청 - 관리자 ID: {}, 문제 ID: {}", adminInfo.id(), id);
+        log.debug("문제 수정 요청 - 관리자 ID: {}, 문제 ID: {}", adminId, id);
         adminProblemService.updateProblem(updateProblemRequest.toServiceDto(id));
         return ApiResponse.ok();
     }
 
     @DeleteMapping("/{id}")
-    public ApiResponse<Void> deleteProblem(@CurrentUser AuthUserInfoDto adminInfo, @PathVariable Long id) {
-        log.debug("문제 삭제 요청 - 관리자 ID: {}", adminInfo.id());
+    public ApiResponse<Void> deleteProblem(@CurrentUser Long adminId, @PathVariable Long id) {
+        log.debug("문제 삭제 요청 - 관리자 ID: {}", adminId);
         adminProblemService.deleteProblem(id);
         return ApiResponse.ok();
     }
